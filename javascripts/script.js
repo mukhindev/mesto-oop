3767b8804215043811625ee1c79e73d61eebdb47
mukhindev <mukhin.dev@gmail.com>
2020-05-24 18:33:13 +0300

    hotfix: замечания ревью

--- javascripts/script.js
+++ javascripts/script.js
@@ -36,23 +36,21 @@ const userProfileButtonAddPlace = userProfile.querySelector('.profile__add-butto
 const popupProfile = document.querySelector('.popup_profile')
 const popupPlace = document.querySelector('.popup_place')
 const popupLightbox = document.querySelector('.popup_lightbox')
+const popupButtonsClose = document.querySelectorAll('.popup__close-button')
 
 // Форма профиля
 const formProfile = popupProfile.querySelector('.popup__form')
 const formInputProfileName = formProfile.querySelector('.popup__input_value_profile-name')
 const formInputProfileAbout = formProfile.querySelector('.popup__input_value_profile-about')
-const formButtonProfileClose = popupProfile.querySelector('.popup__close-button')
 
 // Форма добавления места
 const formPlace = popupPlace.querySelector('.popup__form')
 const formInputPlaceName = formPlace.querySelector('.popup__input_value_place-name')
 const formInputPlacePhoto = formPlace.querySelector('.popup__input_value_place-photo')
-const formButtonPlaceClose = popupPlace.querySelector('.popup__close-button')
 
 // Lightbox
 const lightboxPhoto = popupLightbox.querySelector('.popup__lightbox-photo')
 const lightboxLabel = popupLightbox.querySelector('.popup__lightbox-label')
-const lightboxButtonClose = popupLightbox.querySelector('.popup__close-button')
 
 // Сетка карточек мест
 const places = document.querySelector('.places')
@@ -60,40 +58,57 @@ const places = document.querySelector('.places')
 // Шаблон #place
 const templatePlaceCard = document.querySelector('#place').content
 
-// Добавление карточки места в .places
-function addPlaceCard(name, link) {
+// Функция: Формирование карточки
+function createPlaceCard({ name, link }) {
   const placeCard = templatePlaceCard.cloneNode(true)
   const placeName = placeCard.querySelector('.place__name')
   const placeImage = placeCard.querySelector('.place__photo')
-  placeName.textContent = name
   placeImage.style.backgroundImage = `url("${link}")`
-  places.prepend(placeCard)
+  placeName.textContent = name
+  placeImage.dataset.name = name
+  placeImage.dataset.link = link
+  return placeCard
 }
 
-// Вывод карточек мест
+// Функция: Добавление карточки в .places
+function addPlaceCardInPlaces({ name, link }) {
+  const card = createPlaceCard({ name, link })
+  places.prepend(card)
+}
+
+// Функция: Вывод карточек мест
 initialPlaces.forEach(place => {
-  addPlaceCard(place.name, place.link)
+  addPlaceCardInPlaces(place)
 })
 
+// Функция: Показать попап
+function showPopup(popup) {
+  popup.classList.add('popup_opened')
+}
+
+// Функция: Закрыть всплывающее окно
+function closePopup(popup) {
+  let target
+  if (popup.target) {
+    target = popup.target.closest('.popup_opened')
+  } else {
+    target = popup
+  }
+  target.classList.remove('popup_opened')
+}
+
 // Функция: открывает всплавающее окно и заносит в форму текщие данные
-function showPopup() {
+function showPopupProfile() {
   formInputProfileName.value = userProfileName.textContent
   formInputProfileAbout.value = userProfileAbout.textContent
-  popupProfile.classList.add('popup_opened')
+  showPopup(popupProfile)
 }
 
 // Функция: открывает всплавающее окно и заносит в форму текщие данные
 function showPopupPlace() {
   formInputPlaceName.value = ''
   formInputPlacePhoto.value = ''
-  popupPlace.classList.add('popup_opened')
-}
-
-// Функция: Закрыть всплывающее окно
-function closePopup() {
-  popupProfile.classList.remove('popup_opened')
-  popupPlace.classList.remove('popup_opened')
-  popupLightbox.classList.remove('popup_opened')
+  showPopup(popupPlace)
 }
 
 // Функция: Применить изменения профиля
@@ -101,22 +116,27 @@ function applyСhangesProfile(evt) {
   evt.preventDefault()
   userProfileName.textContent = formInputProfileName.value
   userProfileAbout.textContent = formInputProfileAbout.value
-  closePopup()
+  closePopup(popupProfile)
 }
 
-// Функция: Приминить создание карточки
-function createPlaceCard(evt) {
+// Функция: Создание новой карточки
+function addPlaceCard(evt) {
   evt.preventDefault()
-  addPlaceCard(formInputPlaceName.value, formInputPlacePhoto.value) 
-  closePopup()
+  const place = {
+    name: formInputPlaceName.value,
+    link: formInputPlacePhoto.value
+  }
+  const card = createPlaceCard(place)
+  places.prepend(card)
+  closePopup(popupPlace)
 }
 
 // Функция: Отобразить фото
 function openPhoto(evt) {
-  const photoLink = evt.target.style.backgroundImage.replace(/^url[(]"|"[)]$/g, '')
-  popupLightbox.classList.add('popup_opened')
+  const photoLink = evt.target.dataset.link
   lightboxPhoto.src = photoLink
-  lightboxLabel.textContent = evt.target.nextElementSibling.textContent
+  lightboxLabel.textContent = evt.target.dataset.name
+  showPopup(popupLightbox)
 }
 
 // Функция: Поставить/Снять лайк
@@ -137,11 +157,9 @@ function listenPlaceСard(evt) {
 }
 
 // Слушатели
-userProfileButtonEdit.addEventListener('click', showPopup)
+userProfileButtonEdit.addEventListener('click', showPopupProfile)
 userProfileButtonAddPlace.addEventListener('click', showPopupPlace)
-formButtonProfileClose.addEventListener('click', closePopup)
-formButtonPlaceClose.addEventListener('click', closePopup)
-lightboxButtonClose.addEventListener('click', closePopup)
+popupButtonsClose.forEach(button => button.addEventListener('click', closePopup))
 formProfile.addEventListener('submit', applyСhangesProfile)
-formPlace.addEventListener('submit', createPlaceCard)
+formPlace.addEventListener('submit', addPlaceCard)
 places.addEventListener('click', listenPlaceСard)
